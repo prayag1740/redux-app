fcebfb062c6b1e4c65f6ecd16899534930146701
Prayag Bhatia <prayag.bhatia2015@vit.ac.in>
2022-11-21 13:29:36 +0530

    redux sample app

---
+++ src/components/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div>
+        <h4> Bug Addidas Shoes for Rs 50</h4>
+        <button type="button" class="btn btn-primary mx-2"> - </button>
+        Add this item your cart
+        <button type="button" class="btn btn-primary mx-2"> + </button>
+    </div>
+  )
+}
